80773176416f296524160439b15ad641fb916053
Sardorbek Yorqulov <sardorbekyorqulov1@gmail.com>
2022-11-15 14:06:50 +0500

    Initial commit

---
+++ src/main/java/poputi/uz/poputiuzserverside/PoputiUzServerSideApplication.java
@@ -0,0 +1,13 @@
+package poputi.uz.poputiuzserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoputiUzServerSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoputiUzServerSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/poputi/uz/poputiuzserverside/PoputiUzServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package poputi.uz.poputiuzserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoputiUzServerSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
